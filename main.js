d3d572dcf6004855cdca21f78badf70ec20d06a3
lfatuledickson <lfatuledickson@mail.valenciacollege.edu>
2022-11-17 11:41:27 -0500

    Second commit

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-
 let venom = {
     name: "Venom",
     year: "2018",
     director: "Ruben Fleischer",
     picture: "https://m.media-amazon.com/images/M/MV5BNTFkZjdjN2QtOGE5MS00ZTgzLTgxZjAtYzkyZWQ5MjEzYmZjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_FMjpg_UX1000_.jpg",
 }
-
 let ipMan = {
     name: "Ip Man",
     year: "2008",
     director: "Wilson Yip",
     picture: "https://m.media-amazon.com/images/M/MV5BNTFmMjM3M2UtOTIyZC00Zjk3LTkzODUtYTdhNGRmNzFhYzcyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
 }
-
 let interstellar = {
     name: "Interstellar",
     year: "2014",
@@ -20,51 +17,29 @@ let interstellar = {
     picture: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
 }
 
-const ol = document.querySelector("ol");
+const ul = document.querySelector("ul");
 const myFavoriteMovies = [venom, ipMan, interstellar];
 
 
 
-for(let i = 0; i < myFavoriteMovies.length; i++) {
-        
 
-    const nameKey = myFavoriteMovies.map(function(nameTake) {
-        nameTake = myFavoriteMovies[i].name;
-        return `
-            <li>Name: ${nameTake}</li>
+
+
+    let keys = myFavoriteMovies.map(function(i) {
+       return `
+            <li>Name: ${i.name}</li>
+            <li>Year: ${i.year}</li>
+            <li>Director: ${i.director}</li>
+            <li><img src="${i.picture}"></li>
         `
-    })
-    ol.appendChild(nameKey);
+    });
+
+    let listContent = document.createElement("p");   
+    listContent.innerHTML = keys; 
+
+    ul.appendChild(listContent);
 
 
-    const yearKey = myFavoriteMovies.map(function(yearTake) {
-        yearTake = myFavoriteMovies[i].year;
-        return `
-            <li>Year: ${yearTake}</li>
-        `
-    })
-    ol.appendChild(yearKey);
-
-
-    const directorKey = myFavoriteMovies.map(function(directorTake) {
-        directorTake = myFavoriteMovies[i].director;
-        return `
-            <li>Director: ${directorTake}</li>
-        `
-    })
-    ol.appendChild(directorKey);
-
-
-    const pictureKey = myFavoriteMovies.map(function(pictureTake) {
-        pictureTake = myFavoriteMovies[i].picture;
-        return `
-        <li><img src="${yearTake}"></li>
-    `
-    })
-    ol.appendChild(pictureKey);
-
-
-}
 
 
 
@@ -90,4 +65,29 @@ for(let i = 0; i < myFavoriteMovies.length; i++) {
 
 
 
+// const yearKey = myFavoriteMovies.map(function(yearTake) {
+    //     yearTake = myFavoriteMovies[i].year;
+    //     return `
+    //         <li>Year: ${yearTake}</li>
+    //     `
+    // })
+    // ol.appendChild(yearKey);
+
+
+    // const directorKey = myFavoriteMovies.map(function(directorTake) {
+    //     directorTake = myFavoriteMovies[i].director;
+    //     return `
+    //         <li>Director: ${directorTake}</li>
+    //     `
+    // })
+    // ol.appendChild(directorKey);
+
+
+    // const pictureKey = myFavoriteMovies.map(function(pictureTake) {
+    //     pictureTake = myFavoriteMovies[i].picture;
+    //     return `
+    //     <li><img src="${yearTake}"></li>
+    // `
+    // })
+    // ol.appendChild(pictureKey);
 
